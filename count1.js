28d3cad0022fbf4ed56544317e54e49b51d5a51e
OGHENEMINE CHRISTABEL EDEWOR <66080490+Chrisinabit@users.noreply.github.com>
2021-12-19 07:56:21 +0100

    Add files via upload

---
+++ count1.js
@@ -0,0 +1,36 @@
+
+
+
+const dayEl = document.getElementById("days");
+const hrsEl = document.getElementById("hrs");
+const minEl = document.getElementById("min");
+const secEl = document.getElementById("sec");
+
+const newYear = "1 Jan 2022";
+
+
+function countDown() {
+  const currentDate = new Date();
+      const newYearsDate = new Date(newYear);
+      const totalSeconds = (newYearsDate - currentDate)/1000;
+
+      const seconds = Math.floor(totalSeconds) % 60;
+      const minutes = Math.floor(totalSeconds / 60) % 60;
+      const hours =   Math.floor(totalSeconds / 3600) % 24;
+      const days =    Math.floor(totalSeconds  / 3600 / 24);
+
+      secEl.innerHTML = formatTime(seconds);
+      hrsEl.innerHTML = formatTime(hours);
+      minEl.innerHTML = formatTime(minutes);
+      dayEl.innerHTML = days;
+
+}
+ function formatTime(time){
+   if (time < 10){
+     return time = "0" + time;
+   } else {
+     return time;
+   }
+ }
+ countDown();
+setInterval(countDown, 1000);
